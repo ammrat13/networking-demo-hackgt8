f8cc63645e213002132338a6d841d38dbc13564b
Ammar Ratnani <ammrat13@gmail.com>
2021-10-17 19:39:21 -0400

    Add hello world

--- networking-demo-hackgt8/main.cpp
+++ networking-demo-hackgt8/main.cpp
@@ -1,4 +1,18 @@
+// Win32 API
+#include <Windows.h>
+
+
 int main(void) {
+
+    // Get the console handle so we can print
+    HANDLE stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (stdout == NULL || stdout == INVALID_HANDLE_VALUE) {
+        // Can't print anything, so just die
+        return 1;
+    }
+
+    WriteConsoleA(stdout, "Hello world!\n", 13, NULL, NULL);
+
     // Success
     return 0;
 }
